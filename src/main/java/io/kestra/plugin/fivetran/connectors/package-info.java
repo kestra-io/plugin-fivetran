b0d68ef1bdc869d443401bf5c797743c249b2ed1
Loïc Mathieu <loikeseke@gmail.com>
2023-03-21 14:30:40 +0100

    feat: plugin categories and icons

---
+++ src/main/java/io/kestra/plugin/fivetran/connectors/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for using Fivetran.\n" +
+        "Fivetran is the automated data movement platform moving data out of, into and across your cloud data platforms.",
+    categories = PluginSubGroup.PluginCategory.MISC //FIXME TOOL
+)
+package io.kestra.plugin.fivetran.connectors;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
