f928c87b29f79e0a3ef8eb1b446f2b2e1044f247
ben8t <pimpaudben@gmail.com>
2025-12-30 15:37:21 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/fivetran/connectors/package-info.java
+++ src/main/java/io/kestra/plugin/fivetran/connectors/package-info.java
@@ -1,7 +1,10 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for using Fivetran.\n" +
         "Fivetran is the automated data movement platform moving data out of, into and across your cloud data platforms.",
-    categories = PluginSubGroup.PluginCategory.INGESTION
+    categories = PluginSubGroup.PluginCategory.INGESTION,
+    categories = {
+        PluginSubGroup.PluginCategory.DATA
+    }
 )
 package io.kestra.plugin.fivetran.connectors;
 
